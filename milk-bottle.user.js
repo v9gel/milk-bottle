d72bd5544bf087f4c5b46af62a0d807fd5f2b506
Vyacheslav Nagel <v9gel@mail.ru>
2023-04-15 01:08:15 +0700

    + support htmlacademy

--- milk-bottle.user.js
+++ milk-bottle.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name                Milk Bottle
 // @namespace           http://tampermonkey.net/
-// @version             0.1.0
+// @version             0.1.1
 // @description         Small improvements to the IDE of some online courses
 // @description:ru      Небольшие улучшения для IDE некоторых онлайн-курсов
 // @author              v9gel
 // @match               https://practicum.yandex.ru/trainer/*/lesson/*
+// @match               https://htmlacademy.ru/courses/*/run/*
 // @icon                https://raw.githubusercontent.com/v9gel/milk-bottle/main/icon-64.png
 // @grant               none
 // @updateURL           https://raw.githubusercontent.com/v9gel/milk-bottle/main/milk-bottle.user.js
@@ -16,15 +17,38 @@
 
 (function () {
   'use strict';
-  document.addEventListener('keydown', (event) => {
-    if ((event.metaKey || event.ctrlKey) && event.key === 's') {
-      event.preventDefault();
 
-      const checkTaskButton = document.querySelector('[data-test-id="check-task-button"]');
+  switch (location.hostname) {
+    case 'practicum.yandex.ru':
+      document.addEventListener('keydown', (event) => {
+        saveHotKeyHandler(event, '[data-test-id="check-task-button"]');
+      });
+      break;
 
-      if (checkTaskButton) {
-        checkTaskButton.click();
-      }
-    }
-  });
+    case 'htmlacademy.ru':
+      document.addEventListener('keydown', (event) => {
+        saveHotKeyHandler(event, '.js--server-check')
+      });
+      break;
+  
+    default:
+      break;
+  }
 })();
+
+/**
+ * Handler for saving hotkeys
+ * @param {KeyboardEvent} event 
+ * @param {string} selectorToClick 
+ */
+function saveHotKeyHandler(event, selectorToClick) {
+  if ((event.metaKey || event.ctrlKey) && event.key === 's') {
+    event.preventDefault();
+
+    const checkTaskButton = document.querySelector(selectorToClick);
+
+    if (checkTaskButton) {
+      checkTaskButton.click();
+    }
+  }
+}
